5da10adc2a6daff939694c7eed85ca64a0c45a9f
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-12 19:11:21 -0700

    Add Header component

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={css.container}>Header</div>
+    </header>
+  );
+};
+
+export default Header;
