4f17e98647ba0518f849914c94e0b9e0fe3e7833
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-12 20:08:21 -0700

    header component setup

--- vite-project/src/components/common/Header.jsx
+++ vite-project/src/components/common/Header.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Header.module.css";
 
-const Header = ({ text = "Default text" }) => {
+const Header = ({
+  text = "Default",
+  textColor = "black",
+  bgColor = "brown",
+}) => {
+  const headerStyles = {
+    backgroundColor: "lightblue",
+    border: "1.5px dashed gray",
+  };
   return (
-    <header>
+    <header style={headerStyles}>
       <div className={styles.container}>{text} Header</div>
     </header>
   );
@@ -12,6 +20,8 @@ const Header = ({ text = "Default text" }) => {
 
 Header.propTypes = {
   text: PropTypes.string.isRequired,
+  textColor: PropTypes.string,
+  bgColor: PropTypes.string,
 };
 
 export default Header;
